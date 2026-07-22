5433e52a2d500e12acc9517fcf2f4788d0270c3d
harshitmishra06 <06mishra.harshit@gmail.com>
2026-07-22 22:47:34 +0530

    Loops1 video completed

---
+++ Programming 101/loops1.js
@@ -0,0 +1,136 @@
+//  for(i=0;i<20;i++){
+//      console.log("Hello World");
+// }
+
+// for(; ;){
+//     console.log("HW");
+// }
+
+// function greet(i){
+//     console.log("Namaste");
+// }
+// for(i=0;i<10;i++){
+// greet(i)
+// }
+
+// let sum=0;
+// let arr=[10,6,2,0,8,3];
+// for(let i=0;i<arr.length;i++){
+//     // console.log(arr[i]);
+//     sum=sum+arr[i];
+
+// }
+// console.log(sum);
+
+// let esum = 0;
+// let diff = 0;
+// let osum = 0;
+// let arr = [10, 6, 2, 0, 8, 3, 80, 7, 9];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr[i]);
+// esum=esum+arr[i];
+// }
+// else{
+//     osum=osum+arr[i];
+// }
+// }
+// console.log(esum);
+// console.log(osum);
+// diff=esum-osum;
+// console.log(diff);
+
+// let esum1=0;
+// let osum1=0;
+
+// let arr1=[1,2,3,4,5];
+// let arr2=[6,7,8,9];
+// for(let i=0;i<arr1.length;i++){
+//     if(arr1[i]%2==0){
+
+// esum1=esum1+arr1[i];
+//     }
+//     else{
+//         osum1=osum1+arr1[i];
+//     }
+
+// }
+
+// for(let i=0;i<arr2.length;i++){
+//     if(arr2[i]%2==0){
+
+// esum1=esum1+arr2[i];
+//     }
+//     else{
+//         osum1=osum1+arr2[i];
+//     }
+
+// }
+// console.log(esum1);
+// console.log(osum1);
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [6, 7, 8, 9];
+// let esum = 0;
+// let diff = 0;
+// let osum = 0;
+
+// for (let i = 0; i < arr1.length; i++) {
+    
+  
+//     console.log(arr1[i],arr2[i]);
+//     if (arr1[i] % 2 == 0 ) {
+//         esum = esum + arr1[i] 
+//       } else {
+//         osum = osum + arr1[i] 
+//       }
+//       if (arr2[i]!==undefined){
+//       if (arr2[i] % 2 == 0 ) {
+//         esum = esum + arr2[i] 
+//       } else {
+//         osum = osum + arr2[i] 
+//       }
+//     }
+//   }
+
+// diff = osum - esum;
+// console.log(diff,esum,osum);
+
+
+
+
+
+
+
+
+
+
+// let arr =[1,2,3,4,5]
+// let arr2 = [6,7,8,9]
+
+// let num = arr.concat(arr2)
+
+// let evensum = 0
+// let oddsum = 0
+// // const solution = (num) =>{
+//     for(let i = 0 ; i < num.length ; i++){
+//         if(num[i]%2 == 0){
+//             evensum = evensum + num[i]
+//         }else{
+//             oddsum = oddsum + num[i]  
+//         }
+//     }
+// // }
+
+// // solution(newarr)
+// // console.log(newarr)
+// console.log(evensum,oddsum)
+
+
+
+
+let i=0;
+while(i<5){
+    console.log("HEllo");
+    i++;
+}
--- Programming 101/main.js
+++ Programming 101/main.js
@@ -1,2 +1,114 @@
-console.log("Hello world")
-console.log("hello I am harshit");
+// console.log("Hello world");
+// console.log("hello I am harshit");
+// const a = 10;
+// const b = 20;
+// const sum = a + b;
+// console.log(sum);
+
+// let x = 90;
+// console.log(x);
+
+// let firstName = "Harshit";
+// let lastName = "Mishra";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+
+// let obj = {
+//   name: "Harshit",
+//   age: 21,
+//   arr: [2, 3, 4, [6, 7, "Mishra"]],
+// };
+// // console.log(obj.name + " " + obj.arr[3][2]);
+
+// // function printHelloWorld(name) {
+// //   console.log("Hello," + name);
+// // }
+// // console.log(printHelloWorld());
+// // printHelloWorld("Harshit");
+// function add(a, b) {
+//   sum = a + b;
+//   console.log(sum);
+// }
+// add(obj.arr[3][0],obj.arr[3][1]);
+// add(2, 3);
+// add(10, 20);
+
+
+// function square(x){
+//     let ans=x*x;
+//     return ans;
+// }
+// let val=square(4);
+// console.log(val);
+
+
+
+// function calVal(x,y){
+//     let obj={
+//         add:x+y,
+//         subtract:x-y,
+//         multi:x*y,
+//         div:x/y,
+// }
+// return obj;
+
+// }
+// let abc =calVal(10,2);
+// console.log(abc);
+// console.log(abc.add + abc.subtract);
+
+
+// function agee(age){
+//     if (age>=18){
+//       return "Eligible"
+//     }
+//     else{
+//         return "not eligible"
+//     }
+// }
+// let val=agee(12);
+// console.log(val);
+
+
+
+
+// function evenOdd(num){
+//     if(num%2==0){
+//         return "Num is even";
+//     }
+//     else{
+//         return "Num is odd"
+//     }
+// }
+// let val=evenOdd(-2);
+// console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

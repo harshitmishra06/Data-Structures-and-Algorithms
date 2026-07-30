8e5fa580686beb7f4c940ee403bc1f9e51b8a7d3
harshitmishra06 <06mishra.harshit@gmail.com>
2026-07-30 18:35:30 +0530

    Recursion

--- Arrays/removeDuplicates.js
+++ Arrays/removeDuplicates.js
@@ -1,14 +1,14 @@
-// function removeDuplicate(nums) {
-//   let x = 0;
+function removeDuplicate(nums) {
+  let x = 0;
 
-//   if (nums.length <= 1) return 0;
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] > nums[x]) {
-//       x = x + 1;
-//       nums[x] = nums[i];
-//     }
-//   }
-//   return x + 1;
-// }
-// let res = removeDuplicate([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
-// console.log(res);
+  if (nums.length <= 1) return 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
+    }
+  }
+  return x + 1;
+}
+let res = removeDuplicate([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+console.log(res);
--- Recursion/recursionAndCallStack.js
+++ Recursion/recursionAndCallStack.js
@@ -21,3 +21,79 @@
 
 
 
+// print n to 1 using recursion
+
+// function print(n){
+//     if(n<1) return;
+//     console.log(n);
+//     print(--n);
+// }
+// print(5);
+
+// ---------------------Print 1 to n using recursion-----------------------------
+
+// let x=1;
+// function xyz(n){
+// function func(x){
+//     if(x===n+1) return ;
+//     console.log(x);
+//     func(++x);  
+// }
+// func(x)
+// }
+// xyz(10);
+
+
+// METHOD-2
+
+// let n=10;
+// function print(x){
+//     if(x>n) return;
+//     console.log(x);
+//     print(++x);
+// }
+// print(1);
+
+
+/*------------------------Table of n ------------------------------------------*/
+ 
+
+// let n=5;
+// function print(x){
+//     if(x>n) return;
+//     console.log(x*n);
+//     print(++x);
+// }
+// print(1);
+
+
+/*-----------------------Sum of first n numbers---------------------------------------------*/
+
+// function print(n,sum){
+//     if(n<1) return sum;
+//   sum+=n;
+//    return print(--n,sum);
+
+// }
+// const val=print(10,0);
+// console.log(val);
+
+
+// METHOD-2
+
+// function sum(n){
+//     if(n==0) return 0;
+//     return n+ sum(n-1);
+// }
+// const val=sum(5);
+// console.log(val);
+
+
+/*---------------------Factorial of n -------------------------------*/
+
+function mul(n){
+    if(n==0) return 1;
+    return n* mul(n-1);
+}
+const val=mul(5);
+console.log(val);

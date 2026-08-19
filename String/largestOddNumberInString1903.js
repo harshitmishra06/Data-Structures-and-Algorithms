fe56a83bbb9dedf7dfdd0b7bc2a4bbc931f33147
harshitmishra06 <06mishra.harshit@gmail.com>
2026-08-19 17:04:44 +0530

    largest odd number in string1903

---
+++ String/largestOddNumberInString1903.js
@@ -0,0 +1,9 @@
+var largestOddNumber = function (num) {
+  let n = num.length - 1;
+  while (n >= 0) {
+    if (Number(num[n]) % 2 === 1) {
+      return num.substring(0, n + 1);
+    } else n--;
+  }
+  return "";
+};

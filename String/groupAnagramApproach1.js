baa93086d033cf69c86924c4703b5d6a27d30b7a
harshitmishra06 <06mishra.harshit@gmail.com>
2026-08-22 21:51:35 +0530

    Group anagram approach 2

--- String/groupAnagramApproach1.js
+++ String/groupAnagramApproach1.js
@@ -1,13 +1,15 @@
-var groupAnagrams = function(strs) {
-    let map={};
-    for(let i=0;i<strs.length;i++){
-        let sortedStrs=strs[i].split("").sort().join("");
-        if(!map[sortedStrs]){
-            map[sortedStrs]=[strs[i]];
-        }
-        else{
-            map[sortedStrs].push(strs[i]);
-        }
+var groupAnagrams = function (strs) {
+  let map = {};
+  for (let i = 0; i < strs.length; i++) { // n 
+    let sortedStrs = strs[i].split("").sort().join(""); // for this step time complexity will be- m log m
+    if (!map[sortedStrs]) {
+      map[sortedStrs] = [strs[i]];
+    } else {
+      map[sortedStrs].push(strs[i]);
     }
-    return [...Object.values(map)];
+  }
+  return [...Object.values(map)];
 };
+// for this whole the time complexity will be (n*m log m ).
+// where n is ths number of strings and m is the max length of each string.
+// Space complexity will be O(n m ).

80fb4a73c6c2526abe14cd5aff2abe27234e2e65
harshitmishra06 <06mishra.harshit@gmail.com>
2026-08-22 23:49:27 +0530

    String completed

--- String/groupAnagramApproach2.js
+++ String/groupAnagramApproach2.js
@@ -1,14 +1,14 @@
 var groupAnagrams = function (strs) {
   let map = {};
-  for (let i = 0; i < strs.length; i++) {
+  for (let i = 0; i < strs.length; i++) { // O(n)
     let freqArr = Array(26).fill(0);
     let s = strs[i];
-    for (let j = 0; j < s.length; j++) {
+    for (let j = 0; j < s.length; j++) { // O(m)
       let index = s[j].charCodeAt() - "a".charCodeAt();
       freqArr[index]++;
     }
     let key = "";
-    for (let k = 0; k < 26; k++) {
+    for (let k = 0; k < 26; k++) { // O(1)
       key = key + String.fromCharCode(k) + freqArr[k];
     }
     if (!map[key]) {
@@ -19,3 +19,5 @@ var groupAnagrams = function (strs) {
   }
   return [...Object.values(map)];
 };
+
+// time complexity --> O(n*m)

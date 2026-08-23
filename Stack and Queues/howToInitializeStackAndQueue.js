2e5abcf53ea1e4a3bf21d2ac2240abb108a67570
harshitmishra06 <06mishra.harshit@gmail.com>
2026-08-23 21:31:37 +0530

    Stack and Queues

---
+++ Stack and Queues/howToInitializeStackAndQueue.js
@@ -0,0 +1,42 @@
+// let stack=[];
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// stack.push(4);
+// stack.push(5);
+// stack.push(6);
+// stack.push(7);
+// console.log(stack);
+// stack.pop();
+// stack.pop();
+// stack.pop();
+// stack.pop();
+// console.log(stack);
+// let top=stack.length-1;
+// console.log(stack[top]);
+
+// in stack do not use stack with random index.
+
+/*-----------------------------------Queue-----------------------------*/
+
+
+// let queue=[];
+// queue.push(1);
+// queue.push(2);
+// queue.push(3);
+// queue.push(4);
+// queue.push(5);
+// queue.push(6);
+// console.log(queue);
+// queue.shift();
+// queue.shift();
+// queue.shift();
+// queue.shift();
+// console.log(queue);
+// let front=queue[0];
+// console.log(front);
+
+
+// in queue do not perform queue.pop
+
+

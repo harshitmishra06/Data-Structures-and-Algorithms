9f005db2bc71ce882f22f6d541dc72d47bf8e721
harshitmishra06 <06mishra.harshit@gmail.com>
2026-09-06 21:59:56 +0530

    Next greater element II without double the array

--- Stack and Queues/nextGreaterElementII503.js
+++ Stack and Queues/nextGreaterElementII503.js
@@ -18,3 +18,27 @@ var nextGreaterElements = function (nums) {
   }
   return ans.slice(0, l);
 };
+
+
+/*--------------------------------------------------------------*/
+
+var nextGreaterElements = function (arr) {
+    let stack = [];
+    let n = arr.length;
+    let ans = Array(n).fill(-1);
+    stack.push(arr[n - 1]);
+    for (let i = (2*n) - 2; i >= 0; i--) {
+        while (stack.length) {
+            if (arr[i%n] < stack[stack.length - 1]) {
+                ans[i%n] = stack[stack.length - 1];
+                break;
+            }
+            else {
+                stack.pop();
+            }
+
+        }
+        stack.push(arr[i%n])
+    }
+return ans.slice(0,n);
+};

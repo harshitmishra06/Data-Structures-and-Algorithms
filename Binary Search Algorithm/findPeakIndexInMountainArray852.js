67a7359aeeaab4ef57b67c6269751c7e729555fe
harshitmishra06 <06mishra.harshit@gmail.com>
2026-09-12 00:21:15 +0530

    Find peak index in mountain array

---
+++ Binary Search Algorithm/findPeakIndexInMountainArray852.js
@@ -0,0 +1,13 @@
+var peakIndexInMountainArray = function (arr) {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l < r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (arr[m] > arr[m + 1]) {
+      r = m;
+    } else {
+      l = m + 1;
+    }
+  }
+  return l;
+};
--- Binary Search Algorithm/findPositionOfFirstAndLastElementInSortedArray34.js
+++ Binary Search Algorithm/findPositionOfFirstAndLastElementInSortedArray34.js
@@ -29,3 +29,37 @@ var searchRange = function (nums, target) {
   }
   return ans;
 };
+
+/*----------------------Approach 2---------------------------------------*/
+
+var searchRange = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+  let ans = [-1, -1];
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (nums[m] === target) {
+      ans[0] = m;
+      r = m - 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  l = 0;
+  r = nums.length - 1;
+
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (nums[m] === target) {
+      ans[1] = m;
+      l = m + 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  return ans;
+};

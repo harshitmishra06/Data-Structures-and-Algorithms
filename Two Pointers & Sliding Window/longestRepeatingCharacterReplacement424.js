8dae5c1e95b0810700c4cf53cb34dbe413150097
harshitmishra06 <06mishra.harshit@gmail.com>
2026-09-21 19:14:19 +0530

    longest repeating character replacement 424

---
+++ Two Pointers & Sliding Window/longestRepeatingCharacterReplacement424.js
@@ -0,0 +1,67 @@
+/*--------------Using Map-----------------------*/
+var characterReplacement = function (s, k) {
+  let i = 0;
+  let j = 0;
+  let map = new Map();
+  map.set(s[0], 1);
+  let maxWindow = 0;
+  while (j < s.length) {
+    if (isWindowValid(map, k)) {
+      maxWindow = Math.max(maxWindow, j - i + 1);
+      j++;
+      if (map.has(s[j])) {
+        map.set(s[j], map.get(s[j]) + 1);
+      } else {
+        map.set(s[j], 1);
+      }
+    } else {
+      map.set(s[i], map.get(s[i]) - 1);
+      i++;
+    }
+  }
+  return maxWindow;
+};
+
+var isWindowValid = function (map, k) {
+  let totalCount = 0;
+  let maxCount = 0;
+  for (let i = 0; i < 26; i++) {
+    let char = String.fromCharCode(i + 65);
+    if (map.has(char)) {
+      totalCount += map.get(char);
+      maxCount = Math.max(maxCount, map.get(char));
+    }
+  }
+  return totalCount - maxCount <= k;
+};
+
+/*---------------------Using Array-------------------------------------*/
+
+var characterReplacement = function (s, k) {
+  let i = 0;
+  let j = 0;
+  let arr = Array(26).fill(0);
+  arr[s[0].charCodeAt(0) - 65] = 1;
+  let maxWindow = 0;
+  while (j < s.length) {
+    if (isWindowValid(arr, k)) {
+      maxWindow = Math.max(maxWindow, j - i + 1);
+      j++;
+      arr[s.charCodeAt(j) - 65]++;
+    } else {
+      arr[s.charCodeAt(i) - 65]--;
+      i++;
+    }
+  }
+  return maxWindow;
+};
+
+var isWindowValid = function (arr, k) {
+  let totalCount = 0;
+  let maxCount = 0;
+  for (let i = 0; i < 26; i++) {
+    totalCount += arr[i];
+    maxCount = Math.max(maxCount, arr[i]);
+  }
+  return totalCount - maxCount <= k;
+};

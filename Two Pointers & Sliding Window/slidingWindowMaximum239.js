80399f7c28e707f52b99916191cce20fe6fd0593
harshitmishra06 <06mishra.harshit@gmail.com>
2026-09-23 00:06:57 +0530

    sliding window maximun approach 2  239

--- Two Pointers & Sliding Window/slidingWindowMaximum239.js
+++ Two Pointers & Sliding Window/slidingWindowMaximum239.js
@@ -27,7 +27,30 @@ var maxSlidingWindow = function (nums, k) {
 // time complexity=O(n*k);
 // space complexity=O(1); except ans Array;
 
+/*-----------------For optimal solution we have to use dequeue to make====>Time complexity=O(n) & space complexity=O(k)------------*/
 
+var maxSlidingWindow = function (nums, k) {
+  let i = 0;
+  let j = 0;
+  let ans = [];
+  let dQ = [];
 
-/*-----------------For optimal solution we have to use dequeue to make====>Time complexity=O(n)-----------------*/
+  // dequeue always maintains max element in front aka monotonic decreasing dequeue
 
+  while (j < nums.length) {
+    while (dQ.length && nums[j] > dQ[dQ.length - 1]) {
+      dQ.pop();
+    }
+    dQ.push(nums[j]);
+    if (j >= k - 1) {
+      ans.push(dQ[0]);
+
+      //if the leftmost element for current window(nums[i]) is largest dQ[0],then remove it
+
+      nums[i] === dQ[0] && dQ.shift();
+      i++;
+    }
+    j++;
+  }
+  return ans;
+};
